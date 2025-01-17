365771711ab48c5fce9982794a098b087ec87d1a
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-01-17 19:00:16 +0100

    added h1 to programm

--- Website.02/Tools/Belohnung/Scripts.js
+++ Website.02/Tools/Belohnung/Scripts.js
@@ -2,19 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
     // Frage den Benutzer nach seiner Entscheidung
     const userResponse = confirm("Akzeptierst du deinen Cookie?");
     
-    // Wähle das passende Bild basierend auf der Antwort
+    // Wähle die Elemente und erstelle neue Inhalte
     const imageContainer = document.getElementById("image-container");
+    
+    const heading = document.createElement("h1");
     const img = document.createElement("img");
     
+    // Ändere Inhalte basierend auf der Antwort
     if (userResponse) {
+      heading.textContent = "Lass dir deinen Cookie schmecken!";
       img.src = "cookie-hacker.webp";
       img.alt = "Cookie Hacker";
     } else {
+      heading.textContent = "Kein Cookie für dich... :(";
       img.src = "no-cookie-hacker.webp";
       img.alt = "No Cookie Hacker";
     }
     
-    // Füge das Bild zur Seite hinzu
+    // Füge die Überschrift und das Bild zur Seite hinzu
+    imageContainer.appendChild(heading);
     imageContainer.appendChild(img);
   });
   
