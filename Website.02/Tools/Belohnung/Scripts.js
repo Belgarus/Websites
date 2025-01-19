ff5fa1167b0c66e44e27ccdf85e5b0749228d5fc
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-01-19 12:09:36 +0100

    changed some comments

--- Website.02/Tools/Belohnung/Scripts.js
+++ Website.02/Tools/Belohnung/Scripts.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Frage den Benutzer nach seiner Entscheidung
+    // Fragt den Benutzer nach seiner Entscheidung
     const userResponse = confirm("Akzeptierst du deinen Cookie?");
     
-    // Wähle die Elemente und erstelle neue Inhalte
+    // Wählt die Elemente und erstelle neue Inhalte
     const imageContainer = document.getElementById("image-container");
     
     const heading = document.createElement("h1");
     const img = document.createElement("img");
     
-    // Ändere Inhalte basierend auf der Antwort
+    // Ändert Inhalte basierend auf der Antwort
     if (userResponse) {
       heading.textContent = "Lass dir deinen Cookie schmecken!";
       img.src = "cookie-hacker.webp";
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
       img.alt = "No Cookie Hacker";
     }
     
-    // Füge die Überschrift und das Bild zur Seite hinzu
+    // Fügt die Überschrift und das Bild zur Seite hinzu
     imageContainer.appendChild(heading);
     imageContainer.appendChild(img);
   });
