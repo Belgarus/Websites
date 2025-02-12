9d61bea6fc9257882857706698018924a97ca22a
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-02-12 19:32:04 +0100

    cleaned up my code

--- Website.02/Informatik/Geheim/Belohnung/Scripts.js
+++ Website.02/Informatik/Geheim/Belohnung/Scripts.js
@@ -1,19 +1,15 @@
-    const userResponse = confirm("Akzeptierst du deinen Cookie?");     // Fragt den Benutzer nach seiner Entscheidung
+// Fragt den Benutzer nach seiner Entscheidung
+const userResponse = confirm("Akzeptierst du deinen Cookie?");
 
-    const PicturePlace = document.getElementById("image-container");    
-    const heading = document.getElementById("header");
-    const img = document.getElementById("picture");
-    
-    // Ändert Inhalte basierend auf der Antwort
-    if (userResponse) {
-      heading.textContent = "Lass dir deinen Cookie schmecken!";
-      img.src = "cookie-hacker.webp";
-    } else {
-      heading.textContent = "Kein Cookie für dich... :(";
-      img.src = "no-cookie-hacker.webp";
-    }
-    
-    // Fügt die Überschrift und das Bild zur Seite hinzu
-    imageContainer.appendChild(heading);
-    imageContainer.appendChild(img);
-  
+// Holt die Elemente aus dem DOM
+const heading = document.getElementById("header");
+const img = document.getElementById("picture");
+
+// Ändert Inhalte basierend auf der Antwort
+if (userResponse) {
+  heading.textContent = "Lass dir deinen Cookie schmecken!";
+  img.src = "cookie-hacker.webp";
+} else {
+  heading.textContent = "Kein Cookie für dich... :(";
+  img.src = "no-cookie-hacker.webp";
+}
